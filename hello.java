e3560901b9fa7db3f8848f77adc293b46c375032
krishnakanth <superkk1991@gmail.com>
2021-01-08 23:13:13 +0530

    commit two

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class Hello {
    public static void main(String[] args) 
-      System.out.println("Hello world from krishnakanth!");   
+      System.out.println("Hello world from krishnakanth!");
+      // change for commit!   
 
    }
 }
